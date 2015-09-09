5e025b74dcaf7af2c6837c8232a7b7d5006769cd
Cristian Pallarés <skyrpex@gmail.com>
2015-09-09 13:18:02 +0200

    Fixes code format

--- bin/build.js
+++ bin/build.js
@@ -7,12 +7,12 @@ var babel = require('babel');
 var file = 'src/index.js';
 
 var rs = fs.createReadStream(file)
-  .pipe(through2(function (chunk, enc, callback) {
+  .pipe(through2(function(chunk, enc, callback) {
     var result = babel.transform(chunk, { loose: 'all' });
 
     this.push(result.code);
 
     callback();
-   }))
+  }))
   .pipe(brfs(file))
   .pipe(fs.createWriteStream('lib/index.js'));
